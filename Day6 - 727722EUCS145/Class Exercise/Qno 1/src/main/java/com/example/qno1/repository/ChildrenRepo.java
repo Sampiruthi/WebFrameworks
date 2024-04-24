16106881ed79e536ef60ff1f67997c2e946fde19
Sampiruthi <124981753+Sampiruthi@users.noreply.github.com>
2024-04-24 10:03:21 +0530

    Add files via upload

---
+++ Day6 - 727722EUCS145/Class Exercise/Qno 1/src/main/java/com/example/qno1/repository/ChildrenRepo.java
@@ -0,0 +1,9 @@
+package com.example.qno1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.qno1.model.Children;
+
+public interface ChildrenRepo extends JpaRepository<Children, Integer> {
+
+}
